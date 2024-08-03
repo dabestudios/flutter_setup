531adc7c1a8fdf8d0b7eda6284b919ab6103f385
danib251 <danib251@gmail.com>
2024-08-03 20:37:33 +0200

    workout_app

--- setup_app/lib/pages/new_page.dart
+++ setup_app/lib/pages/new_page.dart
@@ -74,33 +74,73 @@ class _NewPageState extends State<NewPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
               'Welcome to the New Page!',
               style: TextStyle(fontSize: 24),
             ),
-            const Spacer(),
-            // Display the first exercise
+            const SizedBox(height: 16.0),
+            // Mostrar indicador de carga o los ejercicios
             if (_isLoading)
-              const CircularProgressIndicator()
+              const CircularProgressIndicator() // Indicador de carga mientras se cargan los ejercicios
             else if (globalExercises.isNotEmpty)
-              Column(
-                children: [
-                  Text(
-                    globalExercises[0].name ?? 'No Name',
-                    style: TextStyle(fontSize: 18),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Número de columnas en la cuadrícula
+                    childAspectRatio:
+                        3 / 2, // Relación de aspecto de los elementos
+                    mainAxisSpacing:
+                        10, // Espaciado entre elementos en la dirección principal
+                    crossAxisSpacing:
+                        10, // Espaciado entre elementos en la dirección transversal
                   ),
-                  ...globalExercises[0]
-                          .instructions
-                          ?.map((instruction) => Text(instruction))
-                          .toList() ??
-                      [Text('No Instructions')],
-                ],
+                  itemCount: globalExercises.length,
+                  itemBuilder: (context, index) {
+                    final exercise = globalExercises[index];
+                    return ElevatedButton(
+                      onPressed: () {
+                        // Acción al presionar el botón del ejercicio
+                      },
+                      style: ElevatedButton.styleFrom(
+                        // Color del texto del botón
+                        padding: const EdgeInsets.all(
+                            8.0), // Espaciado interno del botón
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(10.0), // Bordes redondeados
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            exercise.name ?? 'No Name',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            exercise.primaryMuscles.isNotEmpty
+                                ? exercise.primaryMuscles[0]
+                                : 'No Primary Muscle',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               )
             else
               const Text('No Exercises Found'),
-            Spacer(),
             if (_bannerAd != null && _isAdsInitialized)
               Align(
                 alignment: Alignment.bottomCenter,
