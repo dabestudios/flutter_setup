82eee588241e904e8650bf1090d2aa88f70052ba
danib251 <danib251@gmail.com>
2024-08-08 20:57:31 +0200

    Refactor RepsOrWeightEditor widget to improve code organization and readability

---
+++ setup_app/lib/widgets/RepsOrWeightEditor.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class RepsOrWeightEditor extends StatelessWidget {
+  final int value;
+  final void Function(int) onValueChanged;
+  final String label;
+  final bool isReps; // Determina si es para reps o peso
+
+  RepsOrWeightEditor({
+    required this.value,
+    required this.onValueChanged,
+    required this.label,
+    required this.isReps,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: Icon(Icons.remove, size: 15),
+          onPressed: () {
+            if (value > 0) {
+              onValueChanged(value - 1);
+            }
+          },
+        ),
+        SizedBox(
+          width: 60,
+          child: TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: label,
+            ),
+            onChanged: (value) {
+              final intValue = int.tryParse(value) ?? 0;
+              onValueChanged(intValue);
+            },
+            controller: TextEditingController(
+              text: value.toString(),
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.add, size: 15),
+          onPressed: () {
+            onValueChanged(value + 1);
+          },
+        ),
+      ],
+    );
+  }
+}
