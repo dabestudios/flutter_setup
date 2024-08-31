f0610ddca2769196108adba655eb709409af21e3
danib251 <danib251@gmail.com>
2024-08-31 13:52:25 +0200

    #12 Update localization strings for Workout Tracker

--- setup_app/lib/auth/auth_gate.dart
+++ setup_app/lib/auth/auth_gate.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_locales/flutter_locales.dart'; // Asegúrate de tener este import
 import 'package:setup_app/main.dart';
 import 'package:setup_app/pages/home_page.dart';
 
@@ -20,26 +21,30 @@ class AuthGate extends StatelessWidget {
               GoogleProvider(clientId: clientId),
             ],
             headerBuilder: (context, constraints, shrinkOffset) {
-              return const Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  'FlutterFire',
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: LocaleText(
+                  'app_name',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
               );
             },
             subtitleBuilder: (context, action) {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: action == AuthAction.signIn
-                    ? const Text('Welcome to FlutterFire, please sign in!')
-                    : const Text('Welcome to Flutterfire, please sign up!'),
+                child: LocaleText(
+                  action == AuthAction.signIn
+                      ? 'welcome_sign_in'
+                      : 'welcome_sign_up',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
               );
             },
             footerBuilder: (context, action) {
               return const Padding(
                 padding: EdgeInsets.only(top: 16),
-                child: Text(
-                  'By signing in, you agree to our terms and conditions.',
+                child: LocaleText(
+                  'terms_and_conditions',
                   style: TextStyle(color: Colors.grey),
                 ),
               );
